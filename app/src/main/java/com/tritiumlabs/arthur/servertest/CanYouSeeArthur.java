a38447cd07c2e6166d77e67467c984b4978c54ee
Kdowding78 <kdowding78@hotmail.com>
2016-06-30 11:31:02 -0400

    Testing

--- app/src/main/java/com/tritiumlabs/arthur/servertest/CanYouSeeArthur.java
+++ app/src/main/java/com/tritiumlabs/arthur/servertest/CanYouSeeArthur.java
@@ -1,7 +1,10 @@
-package main.java.com.tritiumlabs.arthur.servertest;
+package com.tritiumlabs.arthur.servertest;
 
 /**
  * Created by Kyle on 6/29/2016.
  */
 public class CanYouSeeArthur {
+    int dicks = 5;
+    int faggot = 1;
+    int gay = dicks + faggot;
 }
