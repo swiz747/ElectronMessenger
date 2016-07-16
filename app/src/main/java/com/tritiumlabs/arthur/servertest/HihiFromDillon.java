245db48f81647766126101753b2dcee230b2aeac
Arthur <swiz747@gmail.com>
2016-07-15 22:07:46 -0400

    deleted test files

--- app/src/main/java/com/tritiumlabs/arthur/servertest/HihiFromDillon.java
+++
@@ -1,8 +0,0 @@
-package com.tritiumlabs.arthur.servertest;
-
-/**
- * Created by Fox on 7/15/2016.
- */
-public class HihiFromDillon {
-    //derp
-}
--- app/src/main/java/com/tritiumlabs/arthur/servertest/HihiFromLubLub.java
+++
@@ -1,8 +0,0 @@
-package com.tritiumlabs.arthur.servertest;
-
-/**
- * Created by LadyOliviaTheBeautiful on 7/15/16.
- */
-public class HihiFromLubLub {
-    //derp herp
-}
